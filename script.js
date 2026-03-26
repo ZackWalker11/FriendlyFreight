25e21d7cea441be7f5bbff7196da683ad3434006
Zack Walker <ZackWalker@mail.weber.edu>
2026-03-26 15:26:55 -0400

    Test

--- script.js
+++ script.js
@@ -4,19 +4,3 @@ const nav = document.getElementById("nav");
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
-
-// MOBILE DROPDOWN CLICK
-const dropdowns = document.querySelectorAll(".dropdown");
-
-dropdowns.forEach(drop => {
-    const btn = drop.querySelector(".drop-btn");
-
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        const menu = drop.querySelector(".dropdown-menu");
-
-        menu.style.display =
-            menu.style.display === "block" ? "none" : "block";
-    });
-});
